5780f2f229f301472c2a716b4d608007fa4710e2
simonswartout <simon.swartout@gmail.com>
2023-06-06 11:25:25 -0400

    Squashed commit of the following:
    commit aba6a7f13ccfa42070a154bd0c9e6561fe6bd51f
    Merge: 741a874 6e35274
    Author: Arda Gültekin <36036396+ardagltkn96@users.noreply.github.com>
    Date:   Tue Jun 6 16:53:58 2023 +0300
    
        Merge pull request #3 from XRBootcamp/Ardadev
    
        Ardadev
    
    commit 6e3527481402b3252a5d8989be25b712a71f117d
    Author: Arda Gultekin <arda.gultekin@bites.com.tr>
    Date:   Tue Jun 6 16:50:43 2023 +0300
    
        Gesture Detection and a sample shooting scene added.
    
    commit 0544fdde99a3254684a979e576ef36d6aa8bddcc
    Merge: efb6064 741a874
    Author: Arda Gultekin <arda.gultekin@bites.com.tr>
    Date:   Thu Jun 1 14:27:18 2023 +0300
    
        Merge branch 'dev' of https://github.com/XRBootcamp/Finger-Blasters into Ardadev
    
    commit efb6064362641e7ca65a4716468f140ed27d7194
    Author: Arda Gultekin <arda.gultekin@bites.com.tr>
    Date:   Thu Jun 1 14:24:32 2023 +0300
    
        Pulled remote repo

---
+++ Assets/Examples/Scripts/MyLibrary/RandomizeUniqueNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class RandomizeUniqueNumbers
+{
+    private static List<int> randomList = new List<int>();
+    public static System.Object GenerateRandomList(int min, int max)
+    {
+        for (int i = 0; i < max; i++)
+        {
+            int numToAdd = Random.Range(min, max);
+            while (randomList.Contains(numToAdd))
+            {
+                numToAdd = Random.Range(min, max);
+            }
+            randomList.Add(numToAdd);
+
+        }
+        return randomList;
+    }
+}
