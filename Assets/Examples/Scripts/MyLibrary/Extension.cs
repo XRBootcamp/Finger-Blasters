6e3527481402b3252a5d8989be25b712a71f117d
Arda Gultekin <arda.gultekin@bites.com.tr>
2023-06-06 16:50:43 +0300

    Gesture Detection and a sample shooting scene added.

---
+++ Assets/Examples/Scripts/MyLibrary/Extension.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public static class Extension
+{
+    // Per settare tutti i child di un oggetto in un determinato layer
+    public static void SetChildLayers(this Transform t, int layer)
+    {
+        for (int i = 0; i < t.childCount; i++)
+        {
+            Transform child = t.GetChild(i);
+            child.gameObject.layer = layer;
+            SetChildLayers(child, layer);
+        }
+    }
+
+    // Lo uso per includere in una variabile sola piu elementi
+    public static IEnumerable<Tuple<T1, T2, T3>> ZipSeveral3Element<T1, T2, T3>(this IEnumerable<T1> first, IEnumerable<T2> second, IEnumerable<T3> third)
+    {
+        return first.Zip(second, (a, b) => new { a, b }).Zip(third, (t, c) => Tuple.Create(t.a, t.b, c));
+    }
+
+    public static IEnumerable<Tuple<T1, T2>> ZipSeveral2Element<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second)
+    {
+        return first.Zip(second, (a, b) => Tuple.Create(a, b));
+    }
+
+    // Coroutine che serve per ritardare l'inizio di un altro metodo che si vuole richiamare
+    public static IEnumerator DelayRoutine(float delay, Action actionToDo)
+    {
+        yield return new WaitForSeconds(delay);
+        actionToDo.Invoke();
+    }
+
+    public static void DumpToConsole(object obj)
+    {
+        var output = JsonConvert.SerializeObject(obj);
+        Debug.Log(output);
+    }
+
+    public static Guid ToGuid(this string id)
+    {
+        MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider();
+        byte[] inputBytes = Encoding.Default.GetBytes(id);
+        byte[] hashBytes = provider.ComputeHash(inputBytes);
+
+        return new Guid(hashBytes);
+    }
+}
---
+++ Assets/Examples/Scripts/MyLibrary/Randomizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Randomizer
+{
+
+    public static System.Object GenericRandom(object[] array)
+    {
+
+        return array[Random.Range(0, array.Length)];
+
+    }
+
+    public static System.Object RandomList(List<object> list)
+    {
+
+        return list[Random.Range(0, list.Count)];
+
+    }
+
+    public static int RandomInt(int start, int end)
+    {
+        return Random.Range(start, end);
+    }
+}
